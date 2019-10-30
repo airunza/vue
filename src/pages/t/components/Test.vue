dd7e065f30e01b80da4aecb86915788fec200bdc
airunza <15600029968@163.com>
2019-10-30 11:11:54 +0800

    第一次提交

---
+++ src/pages/t/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ t }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Hello-Parcel',
+  data() {
+    return {
+      t: 'Hello Parcel'
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import './style.less';
+</style>
+
+
